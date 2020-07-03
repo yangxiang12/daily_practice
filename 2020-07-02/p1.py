39ae3d8d7e6a1c47449f9ba22abc373bdcac01d1
杨翔 <yangx@funsun.cn>
2020-07-03 15:22:58 +0800

    取最长的字符串

--- 2020-07-02/p1.py
+++ 2020-07-02/p1.py
@@ -4,18 +4,20 @@
 # @Author : 杨翔
 # @File : p1.py
 
-
-def length_Longest_substring(s):
-    k, res, c_dict = -1, 0, {}
-    for i, c in enumerate(s):
-        if c in c_dict and c_dict[c] > k:
-            k = c_dict[c]
-            c_dict[c] = i
+def solution(s):
+    """
+    K: 更新重复字符的下标
+    res: 保留最大无重复字符长度
+    dic: 保留不存在的字符，并且更新重复的字符下标
+    :param s:
+    :return:
+    """
+    k, res, dic = -1, 0, dict()
+    for idx, val in enumerate(s):
+        if val in dic and dic[val] > k:
+            k = dic[val]
+            dic[val] = idx
         else:
-            c_dict[c] = i
-            res = max(res, i - k)
+            dic[val] = idx
+            res = max(res, idx - k)
     return res
-
-
-a = length_Longest_substring("qwertttqiolq")
-
